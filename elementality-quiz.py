b4bbc8fbc4fd461b4c1ef76d1e5a74ff8f771968
school-rashmi <rashmi20220078@prometheusschool.com>
2024-05-30 20:36:56 +0530

    Update elementality-quiz.py

--- elementality-quiz.py
+++ elementality-quiz.py
@@ -186,7 +186,7 @@ if st.session_state.show_results:
             st.divider()
         with col2:
             st.divider()
-            st.image("ah-the-element-of-surprise.jpg", use_column_width=True)
+            st.image("ah-element-of-surprise.jpg", use_column_width=True)
             st.header("You're a mix!")
             st.markdown("You're **a mix** of many groups!")
             st.balloons()
